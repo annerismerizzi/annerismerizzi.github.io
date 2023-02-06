fd7a6bc4c4cc7bb9f4dc02188c3b903cc61ac7c3
AnnerisMerizzi <123563090+AnnerisMerizzi@users.noreply.github.com>
2023-02-06 17:30:12 -0300

    VersionFinal
    VersionFinal

---
+++ js/Boton_conocemas1.js
@@ -0,0 +1,7 @@
+function Funcion () {
+location.href= "http://127.0.0.1:5500/PROYECTO%20INTEGRADOR%202/portfolio.html";
+}
+
+function Volver () {
+    location.href= "http://127.0.0.1:5500/PROYECTO%20INTEGRADOR%202/index.html";
+    }
